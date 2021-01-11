8675e010dda11661472e932654d2d23217020013
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-12 00:21:21 +0100

    make a redesign of components structure

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Footer from "./footer"
-import Header from "./header"
+import Footer from "../Footer/footer"
+import Header from "../Header/header"
 import "@fontsource/poppins"
 import "../styles/basic.scss"
 
