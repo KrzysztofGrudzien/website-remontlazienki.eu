914c92e69510ecdf144dc59a330da28ad06adc2e
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-11 21:07:41 +0100

    create footer of page

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="header">
+      <p className="footer__copyright">
+        Copytights 2020. Wszelkie prawa zastrzeżone przez Artur Grudzień
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
