ff4fd7003ec76cb6d3ebceee48b83d2fff9b9480
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-11 21:08:00 +0100

    add header and footer to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const HomePage = () => {
   return (
-    <div>
+    <>
+      <Header />
       <h1>“Z dumą projektujemy i wykańczamy nowoczesne & stylowe łazienki”</h1>
       <p>
         Nasza firma oferujemy usługi wykończeniowo-remontowe łazienek. Idziemy z
@@ -10,7 +13,8 @@ const HomePage = () => {
         o nietuzinkowym wyglądzie, gdzie inwestor może stworzyć niesamowite i
         efektowne wnętrze sam czy korzystając z usług projektantów
       </p>
-    </div>
+      <Footer />
+    </>
   )
 }
 
