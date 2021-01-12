fdef04d1489ee8eec27d7ff6036dbf80fe5f4385
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-12 23:49:04 +0100

    add classes to the tags, style them and add the link button component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import indexContainer from "./index.module.scss"
+import LinkButton from "../components/LinkButton/linkButton"
 
 const HomePage = () => {
   return (
     <Layout>
-      <h1>“Z dumą projektujemy i wykańczamy nowoczesne & stylowe łazienki”</h1>
-      <p>
-        Nasza firma oferujemy usługi wykończeniowo-remontowe łazienek. Idziemy z
-        duchem czasu i wiemy, że od kilku lat nastał trend na tworzenie łazienek
-        o nietuzinkowym wyglądzie, gdzie inwestor może stworzyć niesamowite i
-        efektowne wnętrze sam czy korzystając z usług projektantów
-      </p>
+      <article className={indexContainer.article}>
+        <h2>
+          “Z dumą projektujemy i wykańczamy nowoczesne & stylowe łazienki”
+        </h2>
+        <p className={indexContainer.articleTitle}>
+          Nasza firma oferujemy usługi wykończeniowo-remontowe łazienek. Idziemy
+          z duchem czasu i wiemy, że od kilku lat nastał trend na tworzenie
+          łazienek o nietuzinkowym wyglądzie, gdzie inwestor może stworzyć
+          niesamowite i efektowne wnętrze sam czy korzystając z usług
+          projektantów
+        </p>
+        <LinkButton />
+      </article>
     </Layout>
   )
 }
