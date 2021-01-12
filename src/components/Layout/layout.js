825f668edea87ac734dec49ff7ec9de9830ecbbf
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-12 23:52:19 +0100

    add class to the main container and style there grid templates, add sidebar and additional kind of basic fonts

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,13 +1,19 @@
 import React from "react"
 import Footer from "../Footer/footer"
 import Header from "../Header/header"
+import Sidebar from "../Sidebar/sidebar"
 import "@fontsource/poppins"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/300.css"
 import "../../styles/basic.scss"
+import "../../styles/typography.scss"
+import containerStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={containerStyles.container}>
       <Header />
+      <Sidebar />
       {children}
       <Footer />
     </div>
