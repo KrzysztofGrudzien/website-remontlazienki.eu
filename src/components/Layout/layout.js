316c0a52780ce78ab819de9ba197a29072bac422
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-12 01:49:19 +0100

    change global font size to 62.5% = 10px = 1rem

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import Footer from "../Footer/footer"
 import Header from "../Header/header"
 import "@fontsource/poppins"
-import "../styles/basic.scss"
+import "../../styles/basic.scss"
 
 const Layout = ({ children }) => {
   return (
