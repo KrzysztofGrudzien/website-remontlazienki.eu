4099c221a4fcc29f875fad6e3b9e2e6299c6cfb7
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-13 03:14:46 +0100

    add a gallery structure, svg icons and style them

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
-import indexContainer from "./index.module.scss"
+import indexStyles from "./index.module.scss"
 import LinkButton from "../components/LinkButton/linkButton"
+// import Img from "../assets/images/lazienka6.jpg"
+import IconArrowLeft from "../assets/icons/icon-arrow-left-dark.svg"
+import IconArrowRight from "../assets/icons/icon-arrow-right-dark.svg"
 
 const HomePage = () => {
   return (
     <Layout>
-      <article className={indexContainer.article}>
+      <article className={indexStyles.article}>
         <h2>
           “Z dumą projektujemy i wykańczamy nowoczesne & stylowe łazienki”
         </h2>
-        <p className={indexContainer.articleTitle}>
+        <p className={indexStyles.articleTitle}>
           Nasza firma oferujemy usługi wykończeniowo-remontowe łazienek. Idziemy
           z duchem czasu i wiemy, że od kilku lat nastał trend na tworzenie
           łazienek o nietuzinkowym wyglądzie, gdzie inwestor może stworzyć
@@ -19,6 +22,17 @@ const HomePage = () => {
         </p>
         <LinkButton />
       </article>
+      <div className={indexStyles.slider}>
+        <a className={indexStyles.sliderNavPrev}>
+          <IconArrowLeft />
+        </a>
+        <div className={indexStyles.sliderGallery}>
+          {/* <img src={Img} className={indexStyles.sliderImage}></img> */}
+        </div>
+        <a className={indexStyles.sliderNavNext}>
+          <IconArrowRight />
+        </a>
+      </div>
     </Layout>
   )
 }
