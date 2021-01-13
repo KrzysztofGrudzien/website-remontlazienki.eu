1ff603190723fd15e300f5175285d5f77417a608
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-13 03:11:33 +0100

    install plugins handling images files, config plugin resposible by svg files and add small part of data as metadata property

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  siteMetadata: {
+    title: "remontlazienki.eu - firma remontowo-wykończeniowa",
+    description:
+      "Nasza firma oferujemy usługi wykończeniowo-remontowe łazienek. Idziemy z duchem czasu i wiemy, że od kilku lat nastał trend na tworzenie łazienek o nietuzinkowym wyglądzie, gdzie inwestor może stworzyć niesamowite i efektowne wnętrze sam czy korzystając z usług projektantów",
+    author: "Krzysztof Grudzień",
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
+  ],
 }
