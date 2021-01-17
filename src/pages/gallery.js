80710472eda1033b97ca7a7e11798c1ccddb1cc3
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-18 00:44:33 +0100

    add static content to the page gallery and style it

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,11 +3,12 @@ import Layout from "../components/Layout/layout"
 import galleryStyles from "./pages.module.scss"
 import IconSmallCross from "../assets/icons/icon-cross-small.svg"
 import LinkButton from "../components/LinkButton/LinkButton"
+import LinkButtons from "../components/LinkButtons/LinkButtons"
 
 const GalleryPage = () => {
   return (
     <Layout>
-      <article className={galleryStyles.article}>
+      <article className={galleryStyles.articleGallery}>
         <IconSmallCross
           className={galleryStyles.articleIconSmallCrossLeftTop}
         />
@@ -20,18 +21,28 @@ const GalleryPage = () => {
         <IconSmallCross
           className={galleryStyles.articleIconSmallCrossRightBottom}
         />
-        <h2>
+        <h2 className={galleryStyles.articleGalleryTitle}>
           “Ponad 10 letnie doświadczeni na rynku Polskim oraz zagranicznym”
         </h2>
-        <p className={galleryStyles.articleTitle}>
-          Liczne grono zadowolonych klientów to nasza najlepsza rekomendacja.
-          Dokonujemy pomiarów i wycen gratis. Przedstawiamy portfolio na
-          pierwszym spotkaniu żeby nie być gołosłownym. Jesteśmy fachowcami.
-          Zapraszamy do współpracy!
-        </p>
-        <LinkButton />
+        <div className={galleryStyles.articleGalleryWrapper}>
+          <p className={galleryStyles.articleGalleryDescription}>
+            Liczne grono zadowolonych klientów to nasza najlepsza rekomendacja.
+            Dokonujemy pomiarów i wycen gratis. Przedstawiamy portfolio na
+            pierwszym spotkaniu żeby nie być gołosłownym. Jesteśmy fachowcami.
+            <br />
+            <br />
+            <strong>Zapraszamy do współpracy!</strong>
+          </p>
+          <LinkButton />
+        </div>
       </article>
+      <div className={galleryStyles.randomImage}></div>
+      <div className={galleryStyles.sliderImage}></div>
+      <h2 className={galleryStyles.galleryTitle}>
+        Ostatnio zrealizowane projekty
+      </h2>
       <span className="page-id">03. nasze prace</span>
+      <LinkButtons />
     </Layout>
   )
 }
