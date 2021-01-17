f84c0ba29eb7efb0d1b5e4042695a95d77b659bf
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-17 03:28:47 +0100

    add styles for main info on the top of page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,16 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import galleryStyles from "./pages.module.scss"
+import IconSmallCross from "../assets/icons/icon-cross-small.svg"
+import LinkButton from "../components/LinkButton/LinkButton"
 
 const GalleryPage = () => {
   return (
     <Layout>
-      <h1>“Ponad 10 letnie doświadczeni na rynku Polskim oraz zagranicznym”</h1>
-      <p>
-        Liczne grono zadowolonych klientów to nasza najlepsza rekomendacja.
-        Dokonujemy pomiarów i wycen gratis. Przedstawiamy portfolio na pierwszym
-        spotkaniu żeby nie być gołosłownym. Jesteśmy fachowcami. Zapraszamy do
-        współpracy!
-      </p>
+      <article className={galleryStyles.article}>
+        <IconSmallCross
+          className={galleryStyles.articleIconSmallCrossLeftTop}
+        />
+        <IconSmallCross
+          className={galleryStyles.articleIconSmallCrossLeftBottom}
+        />
+        <IconSmallCross
+          className={galleryStyles.articleIconSmallCrossRightTop}
+        />
+        <IconSmallCross
+          className={galleryStyles.articleIconSmallCrossRightBottom}
+        />
+        <h2>
+          “Ponad 10 letnie doświadczeni na rynku Polskim oraz zagranicznym”
+        </h2>
+        <p className={galleryStyles.articleTitle}>
+          Liczne grono zadowolonych klientów to nasza najlepsza rekomendacja.
+          Dokonujemy pomiarów i wycen gratis. Przedstawiamy portfolio na
+          pierwszym spotkaniu żeby nie być gołosłownym. Jesteśmy fachowcami.
+          Zapraszamy do współpracy!
+        </p>
+        <LinkButton />
+      </article>
+      <span className="page-id">03. nasze prace</span>
     </Layout>
   )
 }
