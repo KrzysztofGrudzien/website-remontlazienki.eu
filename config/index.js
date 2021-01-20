ac320693b799d04d2b64daf256442ea8ff9e8e4d
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-20 23:35:07 +0100

    add global config file

---
+++ config/index.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // ----- WEBSITE SETTINGS ------ //
+
+  author: "Krzysztof Grudzień",
+  siteTitle: "remontlazienki.eu",
+  siteShortTitle: ["remont", "lazienki", ".eu"],
+  siteDescription:
+    "Firma remontowo-wykończeniowa łazienek. Remonty od A do Z. Projekty indywidualne oraz pod projekt",
+  siteUrl: "", // Fill up later
+  siteLanguage: "pl_PL",
+  siteIcon: "", // Fill up later
+  seoTitleSuffix: "remonty łazienek",
+
+  // ------ NAVIGATION SETTINGS ------ //
+
+  navLinks: {
+    menu: [
+      {
+        name: "o nas",
+        url: "/home",
+      },
+      {
+        name: "usługi",
+        url: "/uslugi",
+      },
+      {
+        name: "galeria",
+        url: "/galeria",
+      },
+      {
+        name: "kontakt",
+        url: "/kontakt",
+      },
+    ],
+  },
+  authorLink: [
+    {
+      name: "Krzysztof Grudzień",
+      url: "http://criscode.eu",
+    },
+  ],
+  socialMediaLink: [
+    {
+      name: "facebook.com/dekorilla",
+      url: "http://facebook.com/dekorilla",
+    },
+  ],
+
+  // ------ SOCIAL MEDIA LINKS ------ //
+
+  socialMediaLinks: [
+    {
+      name: "facebook.com/dekorilla",
+      url: "http://facebook.com/dekorilla",
+    },
+  ],
+}
