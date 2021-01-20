d8cccd69bdb8a1269801e2ac8a246e59ff97df99
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-01-20 16:11:50 +0100

    create and add page 404 and private policy

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const page404 = () => {
+  return <h2>Page Not Found</h2>
+}
+
+export default page404
---
+++ src/pages/private-policy.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const policy = () => {
+  return <h2>Polityka prywatności</h2>
+}
+
+export default policy
